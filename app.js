e2c317e1341001573cf78fb162f5f7715f59f32e
MirFaizan01 <faizanmeer322@gmail.com>
2024-03-28 22:46:30 +0530

    created few routes

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const ejs = require("ejs");
 const Listing = require("./models/listings")
+const path = require("path");
 const app = express();
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+
+
+
+
+
+
+
 
 mongoose.connect("mongodb://localhost:27017/wanderlust").then(()=>{
     console.log("database connection succesfull");
@@ -15,8 +27,25 @@ app.get("/",(req,res)=>{
     res.send("root is working")
 })
 
+app.get("/listings", async (req,res)=>{
+     const allListings = await Listing.find({});
+     res.render("./listings/index.ejs",{allListings})
+})
 
-app.get("/testListing",async (req,res)=>{
+
+app.get("/listings/new",(req,res)=>{
+    res.render("./listings/new.ejs");
+})
+
+
+app.get("/listings/:id",async (req,res)=>{
+    let  {id}= req.params
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing})
+
+
+})
+/* app.get("/testListing",async (req,res)=>{
     let sampleListings =  new Listing({
         title:"My new Villa",
         description:"BY the beach",
@@ -27,7 +56,7 @@ app.get("/testListing",async (req,res)=>{
     await sampleListings.save();
     console.log("saved")
     res.send("working")
-});
+}); */
 
 
 app.listen(8080,()=>{
--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const initData = require("./data");
-const Listing = require("../models/listings")
+const initData = require("./data.js");
+const Listing = require("../models/listings.js")
 
 
 mongoose.connect("mongodb://localhost:27017/wanderlust").then(()=>{
@@ -12,7 +12,9 @@ mongoose.connect("mongodb://localhost:27017/wanderlust").then(()=>{
 
 const initDB = async ()=>{
     await Listing.deleteMany({});
+    console.log("data was deleted")
     await Listing.insertMany(initData.data);
     console.log("data was initailized")
+    
 }
 initDB();
