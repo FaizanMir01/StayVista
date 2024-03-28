843e2046534c4a008abc0397c3d394c1ab043abf
MirFaizan01 <faizanmeer322@gmail.com>
2024-03-28 23:32:46 +0530

    created few routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}))
 
 
 
@@ -15,33 +16,44 @@ app.set("views",path.join(__dirname,"views"));
 
 
 
-
+// connection
 mongoose.connect("mongodb://localhost:27017/wanderlust").then(()=>{
     console.log("database connection succesfull");
 }).catch((err)=>{
     console.log(err);
 })
 
-
+// root 
 app.get("/",(req,res)=>{
     res.send("root is working")
 })
-
+// index route
 app.get("/listings", async (req,res)=>{
      const allListings = await Listing.find({});
      res.render("./listings/index.ejs",{allListings})
 })
 
-
+// new Route create new post using this route
 app.get("/listings/new",(req,res)=>{
     res.render("./listings/new.ejs");
 })
 
-
+// show route -->view in detail a post 
 app.get("/listings/:id",async (req,res)=>{
     let  {id}= req.params
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing})
+})
+
+app.post("/listings",async(req,res)=>{
+    const listing = new Listing(req.body.listing);
+
+    await listing.save()
+    res.redirect("/listings");
+
+
+
+
 
 
 })
